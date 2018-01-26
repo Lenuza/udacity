1af046b91e4c2c683aab2f28f3780696d13d6e23
Elena <lenuza@gmail.com>
2018-01-26 20:06:23 +0200

    Add start script to package.json; add spa prop to ws

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const LocalWebServer = require('local-web-server');
 const localWebServer = new LocalWebServer();
 const server = localWebServer.listen({
     port: process.env.PORT,
-    directory: '.'
+    directory: '.',
+    spa: 'Udacity.html'
 });
